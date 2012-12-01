3f277aef164d1831dcd534901b976968b6554476
sivagollapalli <siva@joshsoftware.com>
2012-12-01 22:46:53 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubychallenge::Application.config.secret_token = '7f1c81d8501cf706ba7903b88c87de8be5dc0afc7bfb9e5b70fca6fbb99a68b1ef0124c4e1d4ef39134124dd86831525df798130aad6c53e933391e9852399ce'
